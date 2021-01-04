9d2d0cd93188d785d0cb6260476d24fe6be8e813
Zack <zackary.allnutt@gmail.com>
2021-01-04 14:01:59 +0000

    Refactors and fixes

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const { powerMonitor } = require('electron')
 const exec = require('child_process').exec
 const interval = 300000
+//const interval = 10000
 
 let count = 0
 
@@ -12,11 +13,13 @@ say('starting aware app')
 
 const workingLoop = () => {
   count += 5
-  say(`You have been working for ${count} minutes`)
-  console.log(`You have been working for ${count} minutes`)
+  let msg = `You have been working for ${count} minutes`
+  console.log(msg)
   if (count > 25) {
-    say('Consider taking a break')
+    msg += ', Consider taking a break'
   }
+
+  say( msg );
 }
 
 setInterval(workingLoop, interval)
@@ -25,11 +28,10 @@ powerMonitor.on('resume', () => {
   say('Resuming')
   count = 0
   console.log('resuming')
-  setInterval(workingLoop, interval)
 })
 
 powerMonitor.on('suspend', () => {
   console.log('suspending')
   clearInterval(workingLoop)
   console.log(count)
-})
+ })
