f410311803b047ec15fa75f68c1adc9149deebeb
Zack <zackary.allnutt@gmail.com>
2021-01-04 10:45:34 +0000

    Changes to suspend method for simplar login

--- main.js
+++ main.js
@@ -1,41 +1,35 @@
-const {powerMonitor} = require('electron'); 
- const exec = require('child_process').exec;
-const interval = 300000;
+const { powerMonitor } = require('electron')
+const exec = require('child_process').exec
+const interval = 300000
 
-let count = 0;
-let idleFlag = false;
+let count = 0
 
-// @todo folow this example to stop loop https://stackoverflow.com/questions/1795100/how-to-exit-from-setinterval
-function workingLoop() {
-  console.log('working Loop started');
-  setInterval(function() {
-      
-   let idle = powerMonitor.getSystemIdleTime() // it returns in seconds when I am writing this
-    console.log('Current System Idle Time - ', idle);
-    if( idle > 10 ) {
-      idleFlag = true;
-      count = 0;
-      console.log(`You've become idle`);
-      startIdleLoop();
-      exit;
-    }
-
-    if( idleFlag === true ) {
-      console.log('idle flag is true');
-    }
-
-    count+=5;
-    exec(`say You have been working or ${count} minutes`);
-    console.log(`hello 2`);
-
-  }, 3000); // 5 minute
+function say (msg) {
+  exec('say ' + msg)
 }
 
-workingLoop();
+say('starting aware app')
 
-function startIdleLoop() {
- setInterval( function() {
-  console.log('Idle loop started');
-  }, 3000);
+const workingLoop = () => {
+  count += 5
+  say(`You have been working for ${count} minutes`)
+  console.log(`You have been working for ${count} minutes`)
+  if (count > 25) {
+    say('Consider taking a break')
+  }
 }
 
+setInterval(workingLoop, interval)
+
+powerMonitor.on('resume', () => {
+  say('Resuming')
+  count = 0
+  console.log('resuming')
+  setInterval(workingLoop, interval)
+})
+
+powerMonitor.on('suspend', () => {
+  console.log('suspending')
+  clearInterval(workingLoop)
+  console.log(count)
+})
